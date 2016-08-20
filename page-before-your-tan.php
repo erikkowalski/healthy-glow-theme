faa81b50737879ed296c5d2d8762b67ef4222ff5
erikkowalski <erikkowalski@mac.com>
2016-08-20 14:26:25 -0400

    fixed nav issue, added about-wrap to about sub pages

--- page-before-your-tan.php
+++ page-before-your-tan.php
@@ -2,6 +2,8 @@
 <header class="hero-img">
 	<img class="img-responsive" src="<?php the_field('before_tan_image')?>" alt="Luffa Sponge">
 </header>
+<div class="about-wrap">
 <?php get_template_part('templates/page', 'header'); ?>
 <?php get_template_part('templates/content', 'page'); ?>
 <?php endwhile; ?>
+</div>
