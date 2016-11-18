cad7d7ced026447f2a72df65c36aed9919284324
erikkowalski <erikkowalski@mac.com>
2016-11-17 23:02:52 -0500

    added store info

--- templates/footer.php
+++ templates/footer.php
@@ -1,11 +1,19 @@
 
  <footer class="content-info">
+
+
   <div class="container">
     <?php dynamic_sidebar('sidebar-footer'); ?>
 
   </div>
+  <hr>
+	 <div class="store-info">
+		 <h1>Healthy Glow, Mattapoisett MA</h1>
+		 <p>Healthy Glow is an Organic Airbrush Tanning Salon, located in Mattapoisett MA. Our Street address is 60 Marion Rd ( Route 6 ), to contacts us via phone please call <a href="tel:774-929-5291">774-929-5291</a> </p>
+	 </div>
+	 <div class="copyright">
+		 <p>Copyright &copy; <?php echo date(Y) ?> Healthy Glow Organic Airbrush Tanning Salon</p>
+	 </div>
 </footer>
 
-<div class="copyright">
-	<p>Copyright &copy; <?php echo date(Y) ?> Healthy Glow Organic Airbrush Tanning Salon</p>
-</div>
+
